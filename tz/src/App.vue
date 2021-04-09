31d900568fd3baf01141350d365a46cd07e2e8c2
GulnazKhasanova <viteewato@mail.ru>
2021-04-09 16:03:29 +0400

    09/04/21

---
+++ tz/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <show-header></show-header>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
+  <show-footer></show-footer>
+</template>
+<script>
+import ShopHeader from "src/components/Header";
+import ShopFooter from "src/components/Footer";
+export default {
+components: {
+  ShopHeader,
+  ShopFooter,
+}
+};
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ tz/src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="menu">
+    <ul>
+      <li><img src="../src/assets/img/shape_1_190.png" alt=""> Описание услуги</li>
+      <li><img src="../src/assets/img/shape_2_191.png" alt="">Зачем мне это?</li>
+      <li><img src="../src/assets/img/shape_3_192.png" alt="">Почему мы?</li>
+      <li><img src="../src/assets/img/shape_4_193.png" alt="">А что есть?</li>
+      <li><img src="../src/assets/img/icon_195.png" alt="">А где?</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style lang="scss">
+#menu{
+  width: 100%;
+  min-height: 120px;
+  &{
+    width: 100%;
+    min-height: 120px;
+    display: flex;
+    flex-direction: row;
+    &{
+      display: flex;
+      flex-direction: column;
+    }
+
+  }
+}
+
+</style>
