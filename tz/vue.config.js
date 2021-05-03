1c90fd1cb1bd287946b995d2715741d01b527666
GulnazKhasanova <viteewato@mail.ru>
2021-05-03 20:05:30 +0400

    03.05.21

--- tz/vue.config.js
+++ tz/vue.config.js
@@ -3,24 +3,10 @@ module.exports = {
     css: {
         loaderOptions: {
             scss: {
-                prependData: `@import "~@/assets/scss/style.scss";`,
+                additionalData: `@import  "~@/assets/scss/style.scss";`,
             },
         },
     },
-    chainWebpack: config => {
-        config.module
-            .rule("fonts")
-            .test(/\.(ttf|otf|eot|woff|woff2)$/)
-            .use("file-loader")
-            .loader("file-loader")
-            .tap(options => {
-                options = {
-                    // limit: 10000,
-                    name: '/assets/inglobal/[name].[ext]',
-                }
-                return options
-            })
-            .end()
-    }
+
 
 }
